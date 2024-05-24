1c66e6d99ed1d157553e6e3ca6bea6bff0cbbaed
Logan-Williams <maksimlebedev200.ru@gmail.com>
2024-05-25 00:03:44 +0400

    Добавление Загрязнения Метод для создания коффе () Добавлен Лог Разделение программы на отдельные классы

---
+++ CoffeeMachineClass.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMachine
+{
+   public class CoffeMachine
+ {
+    private bool isOn;
+    public bool isDirty;
+    private List<string> log = new List<string>();
+    private List<CoffeeRecipeClass.CoffeeRecipeClass> recipes;
+   
+    public CoffeMachine()
+    {
+        isOn = false;
+        isDirty = false;
+        recipes = new List<CoffeeRecipeClass.CoffeeRecipeClass>
+        {
+        new CoffeeRecipeClass.CoffeeRecipeClass("Latte", 200, 20, 150),
+        new CoffeeRecipeClass.CoffeeRecipeClass("Espresso", 50, 80, 0)
+        };
+        log = new List<string>();
+    }
+    public void TurnOn()
+    {
+        if (!isOn)
+        {
+            isOn=true;
+            Console.WriteLine("Кофемашинка включена");
+        }
+        else
+        {
+            Console.WriteLine("Кофеммашинка уже включена");
+        }
+    }
+    public void TurnOff()
+    { 
+        if (isOn)
+        {
+            isOn=false;
+            Console.WriteLine("Кофемашинка выключена");
+        }
+        else
+        {
+            Console.WriteLine("Кофемашинка уже выключена");
+        }
+    }
+
+    public void AddRecipe(CoffeeRecipeClass.CoffeeRecipeClass recipe)
+    {
+        recipes.Add(recipe);
+        Console.WriteLine($"Рецепт {recipe.Name} добавлен.");
+    }
+    public void ShowRecipes()
+    {
+        Console.WriteLine("Доступные рецепты");
+        foreach (var recipe in recipes)
+        {
+            Console.WriteLine(recipe);
+        }
+    }
+    public void MakeDirty()
+    {
+        if (!isDirty)
+        {
+            isDirty = true;
+            Console.WriteLine("Кофемашинка загрязнена");
+        }
+        else 
+        {
+            Console.WriteLine("Кофемашинка чистая");
+        }
+    }
+
+    public void MakeCoffe(string recipeName)
+    {
+        if (!isOn)
+        {
+            Console.WriteLine("Кофемашинка выключена, включите ее для испоьзования");
+            return;
+        }
+        if (isDirty)
+        {
+            Console.WriteLine("Кофемашинка загрязнена. Очистите её для приготовления кофе");
+            return;
+        }
+        CoffeeRecipeClass.CoffeeRecipeClass recipe = recipes.Find(r => r.Name.Equals(recipeName, StringComparison.OrdinalIgnoreCase)); // Equals(Object, Object) Определяет, считаются ли равными указанные экземпляры объектов.
+
+            // r.Name.Equals(recipeName, StringComparison.OrdinalIgnoreCase)
+            // — это условие, проверяющее, равняется ли имя рецепта Name у текущего элемента r значению recipeName,
+            // при этом игнорируется регистр символов благодаря использованию StringComparison.OrdinalIgnoreCase
+
+            if (recipe != null)
+        {
+            Console.WriteLine($"Приготовление {recipe.Name}");
+            MakeDirty();
+            log.Add(recipeName);
+            Console.WriteLine($"{recipe.Name} готов.");
+        }
+       else
+        {
+            Console.WriteLine($"Рецепт {recipe.Name} не найден.");
+        }
+    }
+
+    public void ShowLog()
+    {
+        Console.WriteLine("Приготовленые чашки кофе:");
+        foreach (var items in log) 
+        {
+            Console.WriteLine(items);
+        }
+    }
+
+ }
+}
--- Program.cs
+++ Program.cs
@@ -1,95 +1,32 @@
-﻿class Program
+﻿using CoffeeRecipeClass;
+using CoffeeMachine;
+class Program
 {
 
     static void Main(string[] args)
     {
         CoffeMachine myCofe = new CoffeMachine();
+
         myCofe.TurnOn();
+
         //CoffeeRecipe latte = new CoffeeRecipe("Latte", 200,20,150);
         //CoffeeRecipe esprecsso = new CoffeeRecipe("Espresso", 50, 80,0);
         //myCofe.AddRecipe(latte);
         //myCofe.AddRecipe(esprecsso);
+
         myCofe.ShowRecipes();
+
+        myCofe.MakeCoffe("Latte");
+        myCofe.MakeCoffe("Esresso");
+
+        myCofe.ShowLog();
+
         myCofe.TurnOff();
     }
 
 }
 
 
-public class CoffeMachine
-{
-
-    private bool isOn;
-    private List<CoffeeRecipe> recipes = new List<CoffeeRecipe>
-    {
-        new CoffeeRecipe("Latte", 200, 20, 150), 
-        new CoffeeRecipe("Espresso", 50, 80, 0) 
-    };
-    public CoffeMachine()
-    {
-        isOn = false;
-    }
-    public void TurnOn()
-    {
-        if (!isOn)
-        {
-            isOn=true;
-            Console.WriteLine("Кофемашинка включена");
-        }
-        else
-        {
-            Console.WriteLine("Кофеммашинка уже включена");
-        }
-    }
-    public void TurnOff()
-    { 
-        if (isOn)
-        {
-            isOn=false;
-            Console.WriteLine("Кофемашинка выключена");
-        }
-        else
-        {
-            Console.WriteLine("Кофемашинка уже выключена");
-        }
-    }
-
-    public void AddRecipe(CoffeeRecipe recipe)
-    {
-        recipes.Add(recipe);
-        Console.WriteLine($"Рецепт {recipe.Name} добавлен.");
-    }
-    public void ShowRecipes()
-    {
-        Console.WriteLine("Доступные рецепты");
-        foreach (var recipe in recipes)
-        {
-            Console.WriteLine(recipe);
-        }
-    }
-}
-
-
-public class CoffeeRecipe// меню рецептов 
-{
-    public string Name {  get; set; }
-    public int WaterAmound { get; set; }
-    public int CoffeAmound { get; set; }
-    public int MilkAmound { get; set; }
-
-    public CoffeeRecipe(string name, int waterAmoubd, int coffeAmound, int milkAmmound)
-    {
-        Name = name;
-        WaterAmound = waterAmoubd;
-        CoffeAmound = coffeAmound;
-        MilkAmound = milkAmmound;
-    }
-    public override string ToString()
-    {
-        return $"{Name}:Вода - {WaterAmound} мл, Кофе - {CoffeAmound}г, Молоко - {MilkAmound} мл";
-    }
-
-}
 
 
 
@@ -118,21 +55,6 @@ public class CoffeeRecipe// меню рецептов
 
 
 
-//class Coffe
-//{
-//  static void Ingredients()
-//    {
-//        const int milkLatte = 180;
-//        const int espressoLatte = 30;
-//        const int espressoCofe = 30;
-//        const int woterAmericano = 90;
-//        const int espressoAmericano = 60;
-//    }
-//    public void Menu()
-//    {
-//       switch (true)
-//        {
-//            case true:
-//        }   
-//    }
-//}
+
+
+
